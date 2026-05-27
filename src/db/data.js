b8a9c316ff197f6ee1113dca8d3a46389f414da3
islammdsohan603 <islammdsohan603@gmail.com>
2026-05-27 17:20:11 +0600

    frist commit

---
+++ src/db/data.js
@@ -0,0 +1,11 @@
+export const getData = async () => {
+  const res = await fetch('http://localhost:4000/books', {
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+};

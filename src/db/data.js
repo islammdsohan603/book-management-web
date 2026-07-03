38559d6c4fd9ab88760ce97e6ce10917b9dd40cc
islammdsohan603 <islammdsohan603@gmail.com>
2026-07-03 21:35:44 +0600

    all books api call

--- src/db/data.js
+++ src/db/data.js
@@ -3,7 +3,7 @@
 const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export const getData = async () => {
-  const res = await fetch(`${baseUrl}/api/all-books`, {
+  const res = await fetch(`${baseUrl}/api/recent-books`, {
     cache: 'no-store',
   });
 
@@ -13,3 +13,22 @@ export const getData = async () => {
 
   return res.json();
 };
+
+
+export const getAllBooks = async () => {
+  try {
+    const res = await fetch(`${baseUrl}/api/all-books`, {
+      cache: 'no-store',
+    });
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+  } catch (err) {
+    console.error("Error fetching books", err);
+    throw err;
+  }
+  return res.json();
+}
